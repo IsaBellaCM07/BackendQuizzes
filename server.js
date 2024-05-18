7f36abd58df30a2c789a444f81a4b3b11a982ca1
Isabella Cardozo <isabella.cardozom@uqvirtual.edu.co>
2024-05-18 15:44:52 -0500

    Organización de componentes

--- server.js
+++ server.js
@@ -30,12 +30,121 @@ async function connectToDatabase() {
 // Llama a la función para conectar a la base de datos antes de iniciar el servidor
 connectToDatabase()
     .then(() => {
-        // Importa y usa las rutas
-        const studentRoutes = require('./routes/students')(connection);
-        const examRoutes = require('./routes/exams')(connection);
+        // Define las rutas después de que se haya establecido la conexión
 
-        app.use('/api/estudiantes', studentRoutes);
-        app.use('/api/examenes', examRoutes);
+        // Obtener todos los estudiantes
+        app.get('/api/estudiantes', async (req, res) => {
+            try {
+                const resultEst = await connection.execute('SELECT * FROM Estudiante');
+                const estudiantes = resultEst.rows.map(row => ({
+                    ID_ESTUDIANTE: row[0],
+                    NOMBRE: row[1],
+                    APELLIDO: row[2],
+                }));
+                res.json(estudiantes);
+            } catch (error) {
+                console.error('Error al ejecutar la consulta:', error);
+                res.status(500).json({ error: 'Error al obtener datos' });
+            }
+        });
+
+        // Obtener un estudiante específico por ID
+        app.get('/api/estudiantes/:id', async (req, res) => {
+            try {
+                const studentId = req.params.id;
+                const result = await connection.execute('SELECT * FROM Estudiante WHERE id_estudiante = :id', [studentId]);
+                if (result.rows.length === 0) {
+                    res.status(404).json({ error: 'Estudiante no encontrado' });
+                    return;
+                }
+                const student = {
+                    ID_ESTUDIANTE: result.rows[0][0],
+                    NOMBRE: result.rows[0][1],
+                    APELLIDO: result.rows[0][2],
+                };
+                res.json(student);
+            } catch (error) {
+                console.error('Error al ejecutar la consulta:', error);
+                res.status(500).json({ error: 'Error al obtener datos' });
+            }
+        });
+
+        // Obtener exámenes disponibles para un estudiante específico
+        app.get('/api/examsDis/:id', async (req, res) => {
+            try {
+                const studentId = req.params.id;
+                const result = await connection.execute(`SELECT * FROM presentacion_Estudiante p JOIN examen e ON p.examen_id_examen = e.id_examen
+                                                         WHERE p.presentado = 'N' AND p.estudiante_id_estudiante = :id`, [studentId]);
+
+                // Asegúrate de que `result.metaData` está disponible y contiene los nombres de las columnas
+                const columns = result.metaData.map(col => col.name);
+
+                const presentacion = result.rows.map(row => {
+                    let rowObj = {};
+                    columns.forEach((col, index) => {
+                        rowObj[col] = row[index];
+                    });
+                    return rowObj;
+                });
+                console.log(presentacion)
+                res.json(presentacion);
+            } catch (error) {
+                console.error('Error al ejecutar la consulta:', error);
+                res.status(500).json({ error: 'Error al obtener datos' });
+            }
+        });
+
+
+        // Obtener exámenes presentados para un estudiante específico
+        app.get('/api/examsPres/:id', async (req, res) => {
+            try {
+                const studentId = req.params.id;
+                const result = await connection.execute(`SELECT * FROM presentacion_Estudiante p 
+                                                  JOIN examen e ON p.examen_id_examen = e.id_examen
+                                                  WHERE p.presentado = 'S' AND p.estudiante_id_estudiante = :id`, [studentId]);
+
+                // Asegúrate de que `result.metaData` está disponible y contiene los nombres de las columnas
+                const columns = result.metaData.map(col => col.name);
+
+                const presentacion = result.rows.map(row => {
+                    let rowObj = {};
+                    columns.forEach((col, index) => {
+                        rowObj[col] = row[index];
+                    });
+                    return rowObj;
+                });
+
+                res.json(presentacion);
+            } catch (error) {
+                console.error('Error al ejecutar la consulta:', error);
+                res.status(500).json({ error: 'Error al obtener datos' });
+            }
+        });
+
+        // Obtener grupos a los que pertenece un estudiante específico
+        app.get('/api/grupos/:id', async (req, res) => {
+            try {
+                const studentId = req.params.id;
+                const result = await connection.execute(`SELECT * FROM grupo g JOIN estudiante_grupo eg on g.id_grupo = eg.grupo_id_grupo
+                    WHERE eg.estudiante_id_estudiante = :id`, [studentId]);
+
+                // Asegúrate de que `result.metaData` está disponible y contiene los nombres de las columnas
+                const columns = result.metaData.map(col => col.name);
+
+                const grupos = result.rows.map(row => {
+                    let rowObj = {};
+                    columns.forEach((col, index) => {
+                        rowObj[col] = row[index];
+                    });
+                    return rowObj;
+                });
+
+                res.json(grupos);
+            } catch (error) {
+                console.error('Error al ejecutar la consulta:', error);
+                res.status(500).json({ error: 'Error al obtener datos' });
+            }
+        });
 
         // Inicia el servidor Express
         app.listen(port, () => {
